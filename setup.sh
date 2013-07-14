456577ab657abe53abd2f61b8dec6407516cb4ca
Wade Okumura <theprofessional9@gmail.com>
2013-07-13 17:07:27 -1000

    Update setup.sh
    Adding apt-get for vim

--- setup.sh
+++ setup.sh
@@ -26,6 +26,9 @@ sudo apt-add-repository -y ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install -y emacs24 emacs24-el emacs24-common-non-dfsg
 
+# Install vim
+sudo apt-get install -y vim
+
 # git pull and install dotfiles as well
 cd $HOME
 if [ -d ./dotfiles/ ]; then
