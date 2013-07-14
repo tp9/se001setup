b43c365afc7a09fcde54baaff53c9c4678e20862
Wade Okumura <theprofessional9@gmail.com>
2013-07-13 17:05:18 -1000

    Update setup.sh
    Changing clone statement to git@

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/wokumura/dotfiles.git
+git clone git@github.com/wokumura/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
